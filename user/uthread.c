4de65c496fb488c7a43316a20dba26292448cc69
VastRock-Huang <1621690242@qq.com>
2021-10-15 20:25:00 +0800

    fin Lab7-1

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,34 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for thread context switches. - lab7-1
+struct ctx {
+    uint64 ra;
+    uint64 sp;
+
+    // callee-saved
+    uint64 s0;
+    uint64 s1;
+    uint64 s2;
+    uint64 s3;
+    uint64 s4;
+    uint64 s5;
+    uint64 s6;
+    uint64 s7;
+    uint64 s8;
+    uint64 s9;
+    uint64 s10;
+    uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  struct ctx context;       // thread's context - lab7-1
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct ctx *old, struct ctx *new);  // lab7-1
               
 void 
 thread_init(void)
@@ -63,6 +82,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->context, &current_thread->context);   // switch thread - lab7-1
   } else
     next_thread = 0;
 }
@@ -77,6 +97,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  // set thread's function address and thread's stack pointer - lab7-1
+  t->context.ra = (uint64) func;
+  t->context.sp = (uint64) t->stack + STACK_SIZE;
 }
 
 void 
